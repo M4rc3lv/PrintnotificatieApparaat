cb94ac5c82e7437376b927eb6e495749ba57590d
Marcel V <19297663+M4rc3lv@users.noreply.github.com>
2023-07-24 13:15:03 +0200

    Bestandsnaam van printjob in e-mailnotificatie

--- sendmail.php
+++ sendmail.php
@@ -4,13 +4,16 @@ date_default_timezone_set('Europe/Amsterdam');
 $currentTime = date('H:i');
 
 $Naam=$_GET["printer"];
+$Bestand=isset($_GET["file"])? $_GET["file"] : false;
 
-$to = "yoruemailaddress@gmail.com";
+$to = "marcelveldhuijzen@gmail.com";
 $subject = "3D Print is voltooid!";
 $body = "Hoi Marcel,\r\n\r\nJe print-job op printer '$Naam' is voltooid om $currentTime uur.\r\n\r\nGroetjes,\r\nJe 3D-printer.";
+if($_GET["file"])
+ $body = "Hoi Marcel,\r\n\r\nDe print-job van bestand '$Bestand' op printer '$Naam' is voltooid om $currentTime uur.\r\n\r\nGroetjes,\r\nJe 3D-printer.";
 
-$headers = 'From: 3D-printer@yourwebsite.com' . "\r\n" .
-    'Reply-To: 3D-printer@yourwebsite.com' . "\r\n" .
+$headers = 'From: 3D-printer@marcelv.net' . "\r\n" .
+    'Reply-To: 3D-printer@marcelv.net' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 if(mail($to, $subject, $body, $headers)) {
